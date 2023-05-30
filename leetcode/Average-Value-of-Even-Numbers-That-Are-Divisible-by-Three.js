efaf2e284a93d6bf294e2c4c522d564b72160a24
Althaf <althafahammedm626@gmail.com>
2023-05-30 21:46:27 +0530

    May 30

---
+++ leetcode/Average-Value-of-Even-Numbers-That-Are-Divisible-by-Three.js
@@ -0,0 +1,29 @@
+// Given an integer array nums of positive integers, return the average value of all even integers that are divisible by 3.
+
+// Note that the average of n elements is the sum of the n elements divided by n and rounded down to the nearest integer.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,6,10,12,15]
+// Output: 9
+// Explanation: 6 and 12 are even numbers that are divisible by 3. (6 + 12) / 2 = 9.
+// Example 2:
+
+// Input: nums = [1,2,4,7,10]
+// Output: 0
+// Explanation: There is no single number that satisfies the requirement, so return 0.
+
+var averageValue = function(nums) {
+    let count = 0 
+    let sum = 0
+    nums.forEach((el)=>{
+        if(el%2===0 && el%3===0){
+            console.log(el)
+            sum+=el
+            count++
+        }
+    })
+    return count ? Math.floor(sum/count) : 0
+};
